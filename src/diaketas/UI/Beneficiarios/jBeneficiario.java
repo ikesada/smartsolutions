01205ccbe484f396832d6584529edab535b47c42
Portillo Guerrero, Alejandro <alexpg15@hotmail.com>
2012-03-16 19:59:50 +0100

    Merge branch 'master' of https://github.com/ikesada/smartsolutions
    Conflicts:
    	nbproject/private/private.properties
    	nbproject/project.properties
    	src/diaketas/UI/Beneficiarios/jAltaBeneficiario.form
    	src/diaketas/UI/Beneficiarios/jAltaBeneficiario.java
    	src/diaketas/UI/Beneficiarios/jBajaBeneficiario.java
    	src/diaketas/UI/Beneficiarios/jBeneficiario.java
    	src/diaketas/UI/Beneficiarios/jBuscarBeneficiario.form
    	src/diaketas/UI/Beneficiarios/jBuscarBeneficiario.java
    	src/diaketas/UI/Beneficiarios/jConsultarBeneficiario.form
    	src/diaketas/UI/Beneficiarios/jConsultarBeneficiario.java
    	src/diaketas/UI/Beneficiarios/jModificarBeneficiario.form
    	src/diaketas/UI/Beneficiarios/jModificarBeneficiario.java
    	src/diaketas/UI/Donaciones/jDonaciones.form
    	src/diaketas/UI/Donaciones/jDonaciones.java

--- src/diaketas/UI/Beneficiarios/jBeneficiario.java
+++ src/diaketas/UI/Beneficiarios/jBeneficiario.java
@@ -2,28 +2,28 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package diaketas.UI.Diaketas;
+package diaketas.UI.Beneficiarios;
 
+import diaketas.UI.Diaketas.*;
 import diaketas.UI.UI;
 import javax.swing.JPanel;
+
 /**
  *
  * @author kesada
  */
-public class jDiaketas extends javax.swing.JPanel {
+public class jBeneficiario extends javax.swing.JPanel {
 
-    
-    JPanel altaVoluntario;
-    JPanel bajaVoluntario;
-    jBuscarVoluntario buscarVoluntario;
-    
-
+    JPanel panel;
+    jBuscarBeneficiario buscarBeneficiario;
     
     /**
      * Creates new form jDiaketas
      */
-    public jDiaketas() {
+    public jBeneficiario() {
         initComponents();
+                /*Beneficiarios*/
+        
     }
 
     /**
@@ -38,28 +38,23 @@ public class jDiaketas extends javax.swing.JPanel {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
-        jLabel2 = new javax.swing.JLabel();
         jSeparator3 = new javax.swing.JSeparator();
         jLabel3 = new javax.swing.JLabel();
-        jSeparator4 = new javax.swing.JSeparator();
         jPanel2 = new javax.swing.JPanel();
         botonAlta = new javax.swing.JButton();
         botonBaja = new javax.swing.JButton();
         botonConsultar = new javax.swing.JButton();
         botonModificar = new javax.swing.JButton();
-        botonConsultar1 = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-        jLabel1.setText("Diaketas");
-
-        jLabel2.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        jLabel2.setText("Voluntarios");
+        jLabel1.setText("Beneficiarios");
 
         jLabel3.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        jLabel3.setText("ONG");
+        jLabel3.setText("Beneficiarios");
 
         jPanel2.setLayout(new java.awt.GridLayout(1, 0));
 
+        botonAlta.setIcon(new javax.swing.ImageIcon(getClass().getResource("/diaketas/Iconos/aniadir.png"))); // NOI18N
         botonAlta.setText("Alta");
         botonAlta.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -68,6 +63,7 @@ public class jDiaketas extends javax.swing.JPanel {
         });
         jPanel2.add(botonAlta);
 
+        botonBaja.setIcon(new javax.swing.ImageIcon(getClass().getResource("/diaketas/Iconos/borrar.png"))); // NOI18N
         botonBaja.setText("Baja");
         botonBaja.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -76,6 +72,7 @@ public class jDiaketas extends javax.swing.JPanel {
         });
         jPanel2.add(botonBaja);
 
+        botonConsultar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/diaketas/Iconos/consultar.png"))); // NOI18N
         botonConsultar.setText("Consultar");
         botonConsultar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -84,6 +81,7 @@ public class jDiaketas extends javax.swing.JPanel {
         });
         jPanel2.add(botonConsultar);
 
+        botonModificar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/diaketas/Iconos/editar.png"))); // NOI18N
         botonModificar.setText("Modificar");
         botonModificar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -91,8 +89,6 @@ public class jDiaketas extends javax.swing.JPanel {
             }
         });
         jPanel2.add(botonModificar);
-
-        botonConsultar1.setText("Operacion");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -102,14 +98,11 @@ public class jDiaketas extends javax.swing.JPanel {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jSeparator4)
                     .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(botonConsultar1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jSeparator3, javax.swing.GroupLayout.Alignment.LEADING))
-                .addContainerGap(284, Short.MAX_VALUE))
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -123,14 +116,8 @@ public class jDiaketas extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(botonConsultar1, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator4, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(208, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -141,67 +128,49 @@ public class jDiaketas extends javax.swing.JPanel {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void botonAltaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonAltaActionPerformed
-        // TODO add your handling code here:
+        panel = new jAltaBeneficiario();
 
-        altaVoluntario = new jAltaVoluntario();
-
-        UI.jPrincipal.add("AltaVoluntario", altaVoluntario);
-        UI.cl.show(UI.jPrincipal, "AltaVoluntario");
-        
+        UI.jPrincipal.add("AltaBeneficiario", panel);
+        UI.cl.show(UI.jPrincipal, "AltaBeneficiario");
     }//GEN-LAST:event_botonAltaActionPerformed
 
     private void botonBajaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonBajaActionPerformed
-        // TODO add your handling code here:
+        panel = new jBajaBeneficiario();
 
-        bajaVoluntario = new jBajaVoluntario();
-
-        UI.jPrincipal.add("BajaVoluntario", bajaVoluntario);
-        UI.cl.show(UI.jPrincipal, "BajaVoluntario");
-            
-
+        UI.jPrincipal.add("BajaBeneficiario", panel);
+        UI.cl.show(UI.jPrincipal, "BajaBeneficiario");
     }//GEN-LAST:event_botonBajaActionPerformed
 
     private void botonConsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonConsultarActionPerformed
-        // TODO add your handling code here:
+        buscarBeneficiario = new jBuscarBeneficiario("ConsultarBeneficiario");
         
-        buscarVoluntario = new jBuscarVoluntario();
-        buscarVoluntario.consultar();
 
-        UI.jPrincipal.add("ConsultarVoluntario", buscarVoluntario);
-        UI.cl.show(UI.jPrincipal, "ConsultarVoluntario");
+        UI.jPrincipal.add("ConsultarBeneficiario", buscarBeneficiario);
+        UI.cl.show(UI.jPrincipal, "ConsultarBeneficiario");
     }//GEN-LAST:event_botonConsultarActionPerformed
 
     private void botonModificarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botonModificarActionPerformed
-        // TODO add your handling code here:
-        
-        buscarVoluntario = new jBuscarVoluntario();
-        buscarVoluntario.modificar();
+        buscarBeneficiario = new jBuscarBeneficiario("ModificarBeneficiario");
 
-        UI.jPrincipal.add("ModificarVoluntario", buscarVoluntario); 
-        UI.cl.show(UI.jPrincipal, "ModificarVoluntario");
-        
+        UI.jPrincipal.add("ModificarBeneficiario", buscarBeneficiario); 
+        UI.cl.show(UI.jPrincipal, "ModificarBeneficiario");
     }//GEN-LAST:event_botonModificarActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton botonAlta;
     private javax.swing.JButton botonBaja;
     private javax.swing.JButton botonConsultar;
-    private javax.swing.JButton botonConsultar1;
     private javax.swing.JButton botonModificar;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator3;
-    private javax.swing.JSeparator jSeparator4;
     // End of variables declaration//GEN-END:variables
 }
--- src/diaketas/UI/Donaciones/jDonaciones.java
+++ src/diaketas/UI/Donaciones/jDonaciones.java
@@ -31,26 +31,17 @@ public class jDonaciones extends javax.swing.JPanel {
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
-        jLabel2 = new javax.swing.JLabel();
         jSeparator3 = new javax.swing.JSeparator();
         jLabel3 = new javax.swing.JLabel();
-        jSeparator4 = new javax.swing.JSeparator();
-        jPanel2 = new javax.swing.JPanel();
-        botonAlta = new javax.swing.JButton();
-        botonBaja = new javax.swing.JButton();
-        botonConsultar = new javax.swing.JButton();
-        botonModificar = new javax.swing.JButton();
         botonConsultar1 = new javax.swing.JButton();
 
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel1.setText("Donaciones");
 
-        jLabel2.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        jLabel2.setText("Donantes");
-
         jLabel3.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
         jLabel3.setText("Donaciones");
 
+<<<<<<< HEAD
         jPanel2.setLayout(new java.awt.GridLayout(1, 0));
 
         botonAlta.setText("Alta");
@@ -65,6 +56,8 @@ public class jDonaciones extends javax.swing.JPanel {
         botonModificar.setText("Modificar");
         jPanel2.add(botonModificar);
 
+=======
+>>>>>>> b1d4bf1751b969e7d10dd598c0bd28efa736cfa5
         botonConsultar1.setText("Operacion");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
@@ -74,15 +67,17 @@ public class jDonaciones extends javax.swing.JPanel {
             .addComponent(jSeparator1)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jSeparator4)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(botonConsultar1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 90, javax.swing.GroupLayout.PREFERRED_SIZE)
+<<<<<<< HEAD
                     .addComponent(jSeparator3, javax.swing.GroupLayout.Alignment.LEADING))
                 .addContainerGap(284, Short.MAX_VALUE))
+=======
+                    .addComponent(jSeparator3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 684, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
+>>>>>>> b1d4bf1751b969e7d10dd598c0bd28efa736cfa5
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -97,13 +92,7 @@ public class jDonaciones extends javax.swing.JPanel {
                 .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(botonConsultar1, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 72, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator4, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(62, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -114,24 +103,15 @@ public class jDonaciones extends javax.swing.JPanel {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton botonAlta;
-    private javax.swing.JButton botonBaja;
-    private javax.swing.JButton botonConsultar;
     private javax.swing.JButton botonConsultar1;
-    private javax.swing.JButton botonModificar;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator3;
-    private javax.swing.JSeparator jSeparator4;
     // End of variables declaration//GEN-END:variables
 }
