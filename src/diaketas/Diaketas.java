0c498bd0329849cc3af2d72cf8811f0cf320f779
Miguel Quesada <kesadalive@gmail.com>
2012-03-26 22:59:27 +0100

    Cambiada pantalla principal a Diaketas

--- src/diaketas/Diaketas.java
+++ src/diaketas/Diaketas.java
@@ -11,35 +11,14 @@ import javax.swing.UIManager;
 
 public class Diaketas {
     public static void main(String args[]){
-        /*Look & Feel*/
         
-/*
+        /*Look & Feel*/
         try{
            UIManager.setLookAndFeel(new SyntheticaBlueMoonLookAndFeel());
         } catch (Exception e) {
         }
-*/
-        /*Conexión BBDD*/
-       ConexionBD con = new ConexionBD();
-       con.conectarBD();
-       
-       /*EJEMPLO CONEXION
-        Statement instruccion;
-        ResultSet tabla;
-        try {
-            //Crear objeto Statement para realizar queries a la base de datos
-            instruccion = con.conexion().createStatement();
-            //Un objeto ResultSet, almacena los datos de resultados de una consulta
-            tabla = instruccion.executeQuery("SELECT cod , nombre FROM datos");
-            System.out.println("Codigo\tNombre");
-            while(tabla.next())
-                System.out.println(tabla.getInt(1)+"\t"+tabla.getString(2));
-        }
-        catch(SQLException e){ System.out.println(e); }
-        catch(Exception e){ System.out.println(e); }
-        */
-       
-      
+     
+        
         UI ui = new UI();
         ui.setVisible(true);
         
