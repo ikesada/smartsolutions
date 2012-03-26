b5ab039886262905bd0c365e113989e3ae66f135
Miguel Quesada <kesadalive@gmail.com>
2012-03-26 21:36:50 +0100

    Eliminado desactivarUUsuario de la clase Usuario

--- src/diaketas/Usuarios/Historial/Gestor_de_historiales.java
+++ src/diaketas/Usuarios/Historial/Gestor_de_historiales.java
@@ -28,28 +28,8 @@ public class Gestor_de_historiales {
         /*Nueva acción con Dni de voluntario y beneficiario asociado, junto con fecha actual*/
         Accion ac = new Accion(DNI_Voluntario, DNI, Tipo, new Date());
         
-        /*Se guarda la accion en el sistema*/
-        con.conectarBD();
-        /*Convertimos Date para trabajar*/
-        java.sql.Timestamp fecha = new java.sql.Timestamp(ac.Fecha.getTime());
-         try {
-            instruccion = (Statement) con.conexion().createStatement();
-            instruccion.executeUpdate("INSERT INTO Accion(Nombre, Fecha,"
-                                        + "NIF_CIF_Voluntario, NIF_CIF_Usuario) VALUES (\""
-                                        + ac.tipo + "\",\"" + fecha + "\",\"" + ac.DNI_Voluntario+"\",\""+ac.DNI_Usuario+"\")");
-         }
-         /*Captura de errores*/
-         catch(SQLException e){ System.out.println(e); }
-         catch(Exception e){ System.out.println(e);}
-         /*Desconexión de la BD*/
-         finally {
-            if (con.hayConexionBD()) {
-                try {
-                    con.desconectarBD();
-                } catch (SQLException ex) {
-                    Logger.getLogger(ONG.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }
+        /*Añadimos la acción en el sistema*/
+        ONG.agregarAccion(ac);
     }
 }
+    
