3d480b5fa4fc1b1e60d426513a52057231257916
Miguel Quesada <kesadalive@gmail.com>
2012-03-28 23:28:40 +0100

    Quitados todos los statics

---
+++ src/diaketas/diaketas.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diaketas;
+
+import de.javasoft.plaf.synthetica.SyntheticaBlueMoonLookAndFeel;
+import diaketas.Modelo.Gestores.Gestor_de_beneficiarios;
+import diaketas.Modelo.Gestores.Gestor_de_donantes;
+import diaketas.Modelo.Gestores.Gestor_de_historiales;
+import diaketas.Modelo.Gestores.Gestor_de_voluntarios;
+import diaketas.Modelo.ONG.ONG;
+import diaketas.UI.UI;
+import javax.swing.UIManager;
+
+/**
+ *
+ * @author kesada
+ */
+public class diaketas {
+    public static ONG ong;
+    public static Gestor_de_beneficiarios gestorBeneficiarios;
+    public static Gestor_de_historiales gestorHistoriales;
+    public static Gestor_de_voluntarios gestorVoluntarios;
+    public static Gestor_de_donantes gestorDonantes;
+    
+
+    public static void main(String args[]){
+        
+        /*Look & Feel*/
+        try{
+           UIManager.setLookAndFeel(new SyntheticaBlueMoonLookAndFeel());
+        } catch (Exception e) {
+        }
+        
+        /*Creamos los gestores*/
+        ong = new ONG();
+        gestorBeneficiarios = new Gestor_de_beneficiarios();
+        gestorHistoriales = new Gestor_de_historiales();
+        gestorVoluntarios = new Gestor_de_voluntarios();
+        gestorDonantes = new Gestor_de_donantes();
+
+
+        /*Iniciamos la interfaz*/
+        UI ui = new UI();
+        ui.setVisible(true);
+    }
+}
