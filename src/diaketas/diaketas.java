5a1fe6e08a31da4efd269815e21cc39470089065
Portillo Guerrero, Alejandro <alexpg15@hotmail.com>
2012-03-30 00:49:14 +0200

    Final
    Signed-off-by: Portillo Guerrero, Alejandro <alexpg15@hotmail.com>

--- src/diaketas/diaketas.java
+++ src/diaketas/diaketas.java
@@ -4,31 +4,39 @@
  */
 package diaketas;
 
+import de.javasoft.plaf.synthetica.SyntheticaBlueMoonLookAndFeel;
 import diaketas.Modelo.ONG.ONG;
 import diaketas.UI.UI;
+import javax.swing.UIManager;
 
 /**
  *
  * @author kesada
  */
 public class diaketas {
+
     /**
-     * 
+     *
      */
     public static ONG ong;
-    
 
     /**
      * Funcion principal de sistema
+     *
      * @param args
      */
-    public static void main(String args[]){
-        
-       ong = new ONG();
-    
+    public static void main(String args[]) {
 
+        ong = new ONG();
 
-        /*Iniciamos la interfaz*/
+        try {
+            UIManager.setLookAndFeel(new SyntheticaBlueMoonLookAndFeel());
+        } catch (Exception e) {
+        }
+
+        /*
+         * Iniciamos la interfaz
+         */
         UI ui = new UI();
         ui.setVisible(true);
     }
