04e058fa00c6a06ba0dc24a3e7833a7d3a887eea
Miguel Quesada <kesadalive@gmail.com>
2012-03-29 17:12:06 +0100

    Revert "Movidos Gestores a ONG"
    This reverts commit d9c30f7a8a506cdab7cbc01114096c6af85deb1d.

--- src/diaketas/diaketas.java
+++ src/diaketas/diaketas.java
@@ -5,6 +5,10 @@
 package diaketas;
 
 import de.javasoft.plaf.synthetica.SyntheticaBlueMoonLookAndFeel;
+import diaketas.Modelo.Gestores.Gestor_de_beneficiarios;
+import diaketas.Modelo.Gestores.Gestor_de_donantes;
+import diaketas.Modelo.Gestores.Gestor_de_historiales;
+import diaketas.Modelo.Gestores.Gestor_de_voluntarios;
 import diaketas.Modelo.ONG.ONG;
 import diaketas.UI.UI;
 import javax.swing.UIManager;
@@ -15,17 +19,27 @@ import javax.swing.UIManager;
  */
 public class diaketas {
     public static ONG ong;
+    public static Gestor_de_beneficiarios gestorBeneficiarios;
+    public static Gestor_de_historiales gestorHistoriales;
+    public static Gestor_de_voluntarios gestorVoluntarios;
+    public static Gestor_de_donantes gestorDonantes;
     
+
     public static void main(String args[]){
         
         /*Look & Feel*/
-        try{
+/*        try{
            UIManager.setLookAndFeel(new SyntheticaBlueMoonLookAndFeel());
         } catch (Exception e) {
         }
-      
-        /*Creamos gestor principal ONG*/
+*/        
+        /*Creamos los gestores*/
         ong = new ONG();
+        gestorBeneficiarios = new Gestor_de_beneficiarios();
+        gestorHistoriales = new Gestor_de_historiales();
+        gestorVoluntarios = new Gestor_de_voluntarios();
+        gestorDonantes = new Gestor_de_donantes();
+
 
         /*Iniciamos la interfaz*/
         UI ui = new UI();
