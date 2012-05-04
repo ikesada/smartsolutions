3cd0d039d663791ed37e0f50e10c6278a27c46b0
Cesar Cuadros <toroangelao@gmail.com>
2012-05-04 11:01:53 +0200

    añadidas operaciones

--- src/diaketas/Modelo/Gestores/Gestor_de_ofertas.java
+++ src/diaketas/Modelo/Gestores/Gestor_de_ofertas.java
@@ -19,4 +19,49 @@ public class Gestor_de_ofertas implements iGestorOfertas{
     Oferta O;
     
     
+    /**
+     * NIF del Voluntario que esta trabajando actualmente sobre el sistema
+     */
+    String NIF_Voluntario;  
+    
+    
+    
+    
+    
+    
+    /*------------------OPERACIONES------------------*/
+    
+    /**
+     * 
+     * Funcion que devuelve el objeto Oferta con el que trabaja el Gestor
+     * @return Devuelve el objeto Oferta
+     */ 
+    public Oferta consultarDatosOferta()
+    {
+        
+        Oferta datosOferta = O.obtenerDatosOferta();
+
+        return datosOferta;
+    }
+    
+    
+    
+    
+    public void finModificarOferta()
+    {
+        //FALTA CREAR ESTA OPERACION 
+        //registrarOperacionOfertas( NIF_Voluntario, O.cod_oferta, "Modificacion Oferta" );
+    }
+    
+    
+    public Boolean introducirOferta(Oferta nuevosDatosOferta, String dniV)
+    {
+        
+        Boolean error=false;
+        
+        
+        
+        return error;
+    
+    }
 }
