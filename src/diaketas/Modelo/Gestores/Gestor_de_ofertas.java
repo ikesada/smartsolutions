895991f8e29e2413acb3006e3f0c28ce002beff0
Cesar Cuadros <toroangelao@gmail.com>
2012-05-04 10:29:39 +0200

    creadas clases Gestor_de_ofertas, iGestorOfertas

---
+++ src/diaketas/Modelo/Gestores/Gestor_de_ofertas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diaketas.Modelo.Gestores;
+
+import diaketas.Modelo.ONG.Oferta;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Gestor_de_ofertas implements iGestorOfertas{
+    
+    
+    /**
+     * Oferta sobre la que trabaja el Gestor
+     */
+    Oferta O;
+    
+    
+}
---
+++ src/diaketas/Modelo/Gestores/iGestorOfertas.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diaketas.Modelo.Gestores;
+
+/**
+ *
+ * @author Cesar
+ */
+public interface iGestorOfertas {
+    
+}
