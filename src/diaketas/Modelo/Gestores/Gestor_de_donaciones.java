7f9568d2a32959f8a85cdccf3ec57318738f2e3e
Miguel Quesada <kesadalive@gmail.com>
2012-05-04 16:27:42 +0200

    Agregadas algunas operaciones

--- src/diaketas/Modelo/Gestores/Gestor_de_donaciones.java
+++ src/diaketas/Modelo/Gestores/Gestor_de_donaciones.java
@@ -4,6 +4,9 @@
  */
 package diaketas.Modelo.Gestores;
 
+import diaketas.Modelo.ONG.Movimiento;
+import diaketas.Modelo.ONG.ONG;
+
 /**
  *
  * @author Miguel Á. Quesada
@@ -11,7 +14,26 @@ package diaketas.Modelo.Gestores;
  * @author Fran Dios Buitrago
  * @author Alejandro Portillo Guerrero
  */
-public class Gestor_de_donaciones {
+public class Gestor_de_donaciones implements iGestorDonaciones {
+    
+    Movimiento movimientoSeleccionado;
+    String dniV;
+    
+    
+    @Override
+    public Movimiento consultarDatosMovimiento(){
+        return movimientoSeleccionado;
+    }
+    
+    @Override
+    public boolean confirmarMovimiento(String dniv){
+        
+        if(ONG.gestorVoluntarios.comprobarExistenciaVoluntario(dniV)){
+            movimientoSeleccionado.confirmar(dniV);
+            return true;
+        }    
+        return false;
+    }
     
     
 }
---
+++ src/diaketas/Modelo/Gestores/iGestorDonaciones.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diaketas.Modelo.Gestores;
+
+import diaketas.Modelo.ONG.Movimiento;
+
+/**
+ *
+ * @author Miguel Á. Quesada
+ * @author Jesús Carabaño Bravo
+ * @author Fran Dios Buitrago
+ * @author Alejandro Portillo Guerrero
+ */
+public interface iGestorDonaciones {
+    
+    public Movimiento consultarDatosMovimiento();
+    
+    public boolean confirmarMovimiento(String dniv);
+}
