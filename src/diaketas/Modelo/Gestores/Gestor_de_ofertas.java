e1f466fb0f81395e63203f78212029cda30c969d
Cesar Cuadros <toroangelao@gmail.com>
2012-05-04 13:28:33 +0200

    añadidos operaciones DSI

--- src/diaketas/Modelo/Gestores/Gestor_de_ofertas.java
+++ src/diaketas/Modelo/Gestores/Gestor_de_ofertas.java
@@ -4,6 +4,7 @@
  */
 package diaketas.Modelo.Gestores;
 
+import diaketas.Modelo.ONG.ONG;
 import diaketas.Modelo.ONG.Oferta;
 
 /**
@@ -53,15 +54,49 @@ public class Gestor_de_ofertas implements iGestorOfertas{
         //registrarOperacionOfertas( NIF_Voluntario, O.cod_oferta, "Modificacion Oferta" );
     }
     
-    
+    //devuelve true si ha habido algun error
     public Boolean introducirOferta(Oferta nuevosDatosOferta, String dniV)
     {
         
-        Boolean error=false;
+        Boolean error=true;
         
+        Boolean encontradoUsuario = false;
         
+        encontradoUsuario = ONG.gestorDonantes.comprobarDniDonante( nuevosDatosOferta.NIF_CIF_Donante );
+        
+        if( encontradoUsuario )
+        {
+            encontradoUsuario = ONG.gestorVoluntarios.comprobarExistenciaVoluntario( dniV );
+        }
+        
+        if( encontradoUsuario ) //si ha encontrado a los dos usuarios, hago la modificacion sobre la oferta
+        {
+            error = confirmarModificacion(nuevosDatosOferta);   //devuelve true si hay algun error
+        }
         
         return error;
     
     }
+    
+    //devuelve true si hay algun error
+    public Boolean confirmarModificacion(Oferta nuevosDatosOferta)
+    {
+        
+        Boolean hay_error;
+        
+        //la operacion "modificar" devuelve true si todo ha ido bien
+        Boolean exito = O.modificar(nuevosDatosOferta);
+        
+        if( exito )
+        {
+            hay_error=false;
+        }
+        else
+        {
+            hay_error=true;
+        }
+        
+        return hay_error;
+    }
+    
 }
