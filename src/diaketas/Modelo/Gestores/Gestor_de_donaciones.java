d3ea847ab3f3be54fc4fb556533fa85efb3839a7
Miguel Quesada <kesadalive@gmail.com>
2012-05-04 14:00:45 +0200

    Gestor_de_donaciones creado

---
+++ src/diaketas/Modelo/Gestores/Gestor_de_donaciones.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diaketas.Modelo.Gestores;
+
+/**
+ *
+ * @author Miguel Á. Quesada
+ * @author Jesús Carabaño Bravo
+ * @author Fran Dios Buitrago
+ * @author Alejandro Portillo Guerrero
+ */
+public class Gestor_de_donaciones {
+    
+    
+}
