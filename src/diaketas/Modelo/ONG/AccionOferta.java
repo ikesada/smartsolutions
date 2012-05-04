691471671735a9ff6fb16d9ae78bb602fcff9703
Cesar Cuadros <toroangelao@gmail.com>
2012-05-04 10:29:59 +0200

    creadas clases AccionOferta, Oferta

---
+++ src/diaketas/Modelo/ONG/AccionOferta.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diaketas.Modelo.ONG;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Cesar
+ */
+public class AccionOferta {
+    
+    
+    /**
+     *  Cadena que indica el tipo de accion
+     */
+    public String Nombre;
+    
+    
+    /**
+     *  Fecha en la que se realiza la accion
+     */
+    public Date Fecha;
+    
+    
+    /**
+     *  Identificador del Voluntario que ha realizado la accion
+     */
+    public String NIF_CIF;
+    
+    
+    /**
+     *  Identificador de la Oferta sobre la que se ha realizado la accion
+     */
+    public String codOferta;
+    
+    
+}
---
+++ src/diaketas/Modelo/ONG/Oferta.java
@@ -0,0 +1,88 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diaketas.Modelo.ONG;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Cesar
+ */
+public class Oferta {
+    
+    
+    /**
+     * Codigo asociado a la oferta
+     */
+    public int cod_oferta;
+    
+    /**
+     * Tipo de oferta
+     */
+    public String concepto;
+    
+    /**
+     * Fecha en que se emitio la oferta
+     */
+    public Date fecha;
+
+    /**
+     * Indica si la oferta sigue en pie o no
+     */
+    public int activo;
+    
+    /**
+     * Lugar en el que se ofrece el puesto de trabajo
+     */
+    public String poblacion;
+    
+    /**
+     * Total de vacantes disponibles para una oferta
+     */
+    public int numero_vacantes;
+    
+    /**
+     * Breve resumen de los aspectos principales de la oferta
+     */
+    public String descripcion;
+    
+    /**
+     * Estudios o experiencia laboral mínimos requeridos
+     */
+    public String requisitos_minimos;
+    
+    /**
+     * Especifica la clase de contrato ofrecido
+     */
+    public String tipo_contrato;
+    
+    /**
+     * Número de horas diarias de trabajo del puesto ofrecido
+     */
+    public int jornada_laboral;
+    
+    /**
+     * Cantidad a cobrar (mensual) por el puesto ofrecido
+     */
+    public Double salario;
+    
+    /**
+     * Notas adicionales sobre la oferta
+     */
+    public String observaciones;
+    
+    
+    
+    /**
+     * DNI del donante que ha facilitado la oferta
+     */
+    public String NIF_CIF_Donante;
+    
+    
+    
+    
+    
+    
+}
