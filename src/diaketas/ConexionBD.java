cb62f2295b7a268a574234cc5306633d1c037ef0
Miguel Quesada <kesadalive@gmail.com>
2012-05-25 17:36:21 +0200

    Metidos Cambios BBDD: -Se conecta una unica vez al iniciar el programa -Se desconecta al cerrar la UI principal -Se han eliminado todos los bloques finally pues ya no tienen sentido (solo se usaban para cerrar la conexion)

--- src/diaketas/ConexionBD.java
+++ src/diaketas/ConexionBD.java
@@ -8,8 +8,10 @@ package diaketas;
  *
  * @author kesada
  */
+import com.mysql.jdbc.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 /**
  * 
@@ -35,10 +37,8 @@ public class ConexionBD{
     
     private static Connection conn = null;
     
-    /**
-     *  Funcion conecta con la BBDD
-     */
-    public void conectarBD() {
+    
+    public void realizarConexionBD(){
         try {
             Class.forName("org.gjt.mm.mysql.Driver");
             conn = DriverManager.getConnection(url, login, password);
@@ -48,17 +48,28 @@ public class ConexionBD{
         }
         catch(ClassNotFoundException ex) {
             System.out.println(ex);
-        }
+        }        
     }
     
     /**
-     *  Desconecta de la BBDD
-     * @throws SQLException
+     *  Funcion conecta con la BBDD
      */
-    public void desconectarBD() throws SQLException{
-        conn.close();
+    public void conectarBD() {
+      try {
+        Statement stmt = (Statement) conn.createStatement();
+        
+        ResultSet rs = stmt.executeQuery("SELECT 1 FROM Dual");
+        if (rs.next()) {
+        }
+        }catch (SQLException e) {
+            System.out.println("Conexion cerrada...Abriendo");
+            realizarConexionBD();
+        }
     }
     
+    public void realizarDesconexionBD() throws SQLException{
+        conn.close();
+    }
     /**
      *  Realiza la convexion
      * @return Devuelve la conexion a la BBDD
@@ -68,14 +79,4 @@ public class ConexionBD{
         return(conn);
     }
     
-    /**
-     *  Comprueba si hay conexion con la BBDD o no
-     * @return Devuelve True o False si la conexion existe
-     */
-    public boolean hayConexionBD(){
-       if(conn!=null)
-          return true;
-       else
-          return false;        
-    }
 }
